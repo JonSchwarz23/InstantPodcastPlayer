afe1fc50d76941780d248c1fc9f776ed1bf07a2b
Jonathan Schwarz <jschwarz@bfcm.com>
2021-02-17 20:23:54 -0500

    args and what not

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 const { default: axios } = require("axios");
 const xml2js = require("xml2js");
 const fs = require("fs");
+const config = require("../.config.json");
 var player = require('play-sound')(opts = {})
 
-let lastPlayedGuid = "ae978519-4720-4a06-b2ec-30814b0de8d1";
+let feed = null;
+let lastPlayedGuid = null;
+let playOnLoad = false;
+let timeout = 10;
+
 
 const checkForUpdate = async () => {
 	try {
-		const response = await axios.get(
-			"https://feeds.npr.org/500005/podcast.xml"
-		);
+		const response = await axios.get(feed);
 		const json = await xml2js.parseStringPromise(response.data);
 
 		const guid = json.rss.channel[0].item[0].guid[0]._;
@@ -22,6 +25,12 @@ const checkForUpdate = async () => {
 			console.log("GUIDs are different");
 
 			lastPlayedGuid = guid;
+
+			if(!playOnLoad)
+			{
+				playOnLoad = true;
+				return;
+			}
 
 			const url = json.rss.channel[0].item[0].enclosure[0].$.url;
 
@@ -48,6 +57,20 @@ const checkForUpdate = async () => {
 	}
 };
 
+const processArgumentsAndConfig = () => {
+	const argv = require('minimist')(process.argv.slice(2), {boolean: "p"});
+
+	if(config.feed) feed = config.feed;
+	if(config.playOnLoad) playOnLoad = config.playOnLoad;
+	if(config.timeout) timeout = config.timeout;
+
+	if(argv._.length >= 1) feed = argv._[0];
+	if(argv.t) timeout = parseInt(argv.t);
+	if(argv.p) playOnLoad = true;
+
+	if(!feed) throw new Error("Feed must be set either through command line or config file");
+}
+
 const run = async () => {
 	try
 	{
@@ -59,8 +82,9 @@ const run = async () => {
 	}
 	finally
 	{
-		setTimeout(run, 120000);
+		setTimeout(run, timeout * 1000);
 	}
 };
 
+processArgumentsAndConfig();
 run();
